d2ef752e89b974e6c2ecdeb82550f5feb6aa322e
Reese Glasscock <reesegpdx@gmail.com>
2018-08-21 11:08:16 -0700

    adding in firebase

---
+++ src/app/app-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyB_2EGr0-hyTpifcB7XjzunvVG-jAxGQsE",
+  authDomain: "record-store-b314d.firebaseapp.com",
+  databaseURL: "https://record-store-b314d.firebaseio.com",
+  projectId: "record-store-b314d",
+  storageBucket: "",
+  messagingSenderId: "674829977177"
+};
